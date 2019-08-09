aecc569c39861922276533918ca69b2e79a533e6
ismail koroglu <ismailkoroglu@gmail.com>
2019-08-10 00:10:25 +0300

    Json'a bağlı olarak karakter üretimi gerçekleştirildi.

---
+++ Assets/_ismail/Script/Extras/CharacterSpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using Newtonsoft.Json;
+
+public class CharacterSpawner : MonoBehaviour
+{
+
+    public List<Character> characters;
+    void Start()
+    {
+        var path = Application.streamingAssetsPath + "/CharacterSpawnList.json";
+        var fileContent = File.ReadAllText(path);
+        characters = JsonConvert.DeserializeObject<List<Character>>(fileContent);
+        //  Character ch = JsonConvert.DeserializeObject<Character>(fileContent);
+        //   Debug.Log(ch.Name);
+        CreateCharacters();
+    }
+
+    void CreateCharacters()
+    {
+        for (int i = 0; i < characters.Count; i++)
+        {
+            string name = characters[i].Name;
+            Debug.Log(name);
+            GameObject g = (GameObject)Instantiate(Resources.Load(name));
+            g.transform.position = characters[i].Pos;
+            g.SetActive(true);
+        }
+    }
+}
+
+[System.Serializable]
+public class Character
+{
+    public string Name;
+    public Vector3 Pos;
+
+}
---
+++ Assets/_ismail/Script/_etc/UsingJsonDotNetInUnity.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using UnityEngine;
+
+public class UsingJsonDotNetInUnity : MonoBehaviour
+{
+    private void Awake()
+    {
+        var accountJames = new Account
+        {
+            Email = "james@example.com",
+            Active = true,
+            CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+            Roles = new List<string>
+            {
+                "User",
+                "Admin"
+            },
+            Ve = new Vector3(10, 3, 1),
+            StrVector3Dictionary = new Dictionary<string, Vector3>
+            {
+                {"start", new Vector3(0, 0, 1)},
+                {"end", new Vector3(9, 0, 1)}
+            }
+        };
+
+
+        var accountOnion = new Account
+        {
+            Email = "onion@example.co.uk",
+            Active = true,
+            CreatedDate = new DateTime(2013, 1, 20, 0, 0, 0, DateTimeKind.Utc),
+            Roles = new List<string>
+            {
+                "User",
+                "Admin"
+            },
+            Ve = new Vector3(0, 3, 1),
+            StrVector3Dictionary = new Dictionary<string, Vector3>
+            {
+                {"vr", new Vector3(0, 0, 1)},
+                {"pc", new Vector3(9, 9, 1)}
+            }
+        };
+
+
+        var setting = new JsonSerializerSettings();
+        setting.Formatting = Formatting.Indented;
+        setting.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+
+        // write
+        var accountsFromCode = new List<Account> { accountJames, accountOnion };
+        var json = JsonConvert.SerializeObject(accountsFromCode, setting);
+        var path = Path.Combine(Application.dataPath, "hi.json");
+        File.WriteAllText(path, json);
+
+        // read
+        var fileContent = File.ReadAllText(path);
+        var accountsFromFile = JsonConvert.DeserializeObject<List<Account>>(fileContent);
+        var reSerializedJson = JsonConvert.SerializeObject(accountsFromFile, setting);
+
+        print(reSerializedJson);
+        print("json == reSerializedJson is" + (json == reSerializedJson));
+    }
+
+    public class Account
+    {
+        public string Email { get; set; }
+        public bool Active { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public IList<string> Roles { get; set; }
+        public Vector3 Ve { get; set; }
+        public Dictionary<string, Vector3> StrVector3Dictionary { get; set; }
+    }
+}
